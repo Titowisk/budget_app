b16a702406b6fe418db1cbf12ad7235cf3f8f35d
Vitor Filardi <rabelo51@gmail.com>
2019-03-03 16:40:58 -0300

    Created Transactions and Budget models

--- bank_statements_reader/models.py
+++ bank_statements_reader/models.py
@@ -1,3 +1,53 @@
+# django
 from django.db import models
+# python
+from datetime import date
 
-# Create your models here.
+  
+class Budget(models.Model):
+
+    date = models.DateField(unique_for_date=date)
+
+    def __str__(self):
+        return "Budget: {month}/{year}".format(month=self.date.month, year=self.date.year)
+
+class Transactions(models.Model):
+
+    # ?
+    INCOME = '1'
+    EXPENSE = '0'
+
+    EXPENSE_OR_INCOME = (
+        (EXPENSE, 'Expense'),
+        (INCOME, 'Income')
+    )
+
+    # attributes
+    origin = models.CharField(max_length=100)
+    amount = models.DecimalField(decimal_places=2, max_digits=9)
+    flow_method = models.CharField(max_length=50)
+    date = models.DateField()
+    flow_type = models.CharField(max_length=1, choices=EXPENSE_OR_INCOME, default=EXPENSE)
+    slug = models.SlugField()
+
+    # relationships
+    budget = models.ForeignKey('Budget', on_delete=models.CASCADE, related_name='transactions')
+
+
+    # methods
+    def print_flow(self):
+        text = """
+                ==================== CashFlow object ===================
+                Amount: {amount}, Date: {date}, Flow_Method: {flow_method}
+                Origin: {origin} 
+                """.format(
+                    amount = self.amount,
+                    date = "{0}-{1}-{2}".format(self.date.day,self.date.month, self.date.year),
+                    flow_method = self.flow_method,
+                    origin = self.origin
+                )
+        print(text)
+    
+    def __str__(self):
+        return self.origin
+
