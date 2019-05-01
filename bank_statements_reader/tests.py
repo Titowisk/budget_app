ba4bdd8e3af4b21a4d2d23191903639f87ad862a
Vitor Filardi <rabelo51@gmail.com>
2019-05-01 15:22:40 -0300

    Add test_transactions_link

--- bank_statements_reader/tests.py
+++ bank_statements_reader/tests.py
@@ -1,15 +1,42 @@
 from django.test import TestCase
 
-from .models import Month
+from .models import Month, Transaction
+
+from datetime import date
 
 class MonthModelTests(TestCase):
     # https://docs.djangoproject.com/en/2.2/topics/testing/overview/
     # https://docs.djangoproject.com/en/2.2/intro/tutorial05/
 
     def setUp(self):
-        Month.objects.create(month_number="1")
+        """
+        02/01/19; Visa Electron;0161503;;"-17,03";
+        ;Tagarelli;;
 
-    def create_month_instance(self):
+        02/01/19; Visa Electron;0300645;;"-31,68";
+        ;Ponto Verde Supermer;;
+
+        11/02/19; Visa Electron;0090626;;"-17,99";
+        ;Panilha Delicates;;
+        """
+        Month.objects.create(month_number="1")
+        Transaction.objects.create(
+            origin="Tagarelli", statement_number="0161503", amount="-17.03",
+            flow_method="Visa Electron", date=date(19,1,2), month=Month.objects.get(month_number="1")
+        )
+        Transaction.objects.create(
+            origin="Ponto Verde Supermer", statement_number="0300645", amount="-31.68",
+            flow_method="Visa Electron", date=date(19,1,2), month=Month.objects.get(month_number="1")
+        )
+        Transaction.objects.create(
+            origin="Panilha Delicates", statement_number="0090626", amount="-17.99",
+            flow_method="Visa Electron", date=date(19,2,11), month=Month.objects.get(month_number="1")
+        )
+
+    def test_month_instance(self):
         january = Month.objects.get(month_number="1")
         self.assertEqual(january.month_number, "1")
 
+    def test_transactions_link(self):
+        january = Month.objects.get(month_number="1")
+        self.assertEqual(january.transactions.count(), 3)
