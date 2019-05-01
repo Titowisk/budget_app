4ce0dd4553ad670138bc452a84e243aeaa01a9db
Vitor Filardi <rabelo51@gmail.com>
2019-05-01 15:07:55 -0300

    Create Test suite and instance_test for month model

--- bank_statements_reader/tests.py
+++ bank_statements_reader/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Month
+
+class MonthModelTests(TestCase):
+    # https://docs.djangoproject.com/en/2.2/topics/testing/overview/
+    # https://docs.djangoproject.com/en/2.2/intro/tutorial05/
+
+    def setUp(self):
+        Month.objects.create(month_number="1")
+
+    def create_month_instance(self):
+        january = Month.objects.get(month_number="1")
+        self.assertEqual(january.month_number, "1")
+
