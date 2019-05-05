d1a0695f120d32dc57f46d868f64cd5537b54ff8
Vitor Filardi <rabelo51@gmail.com>
2019-05-05 17:20:40 -0300

    Add path with keyword argument year that is handled in views.MonthsByYearList

--- budget_viewer/urls.py
+++ budget_viewer/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from . import views
+# https://docs.djangoproject.com/en/2.2/ref/urls/#path
 
 urlpatterns = [
-    path('transactions', views.TransactionsView.as_view(), name='transactions'),
+    path('transactions', views.YearsView.as_view(), name='transactions'),
+    path('transactions/monthsByYear/<year>', views.MonthsByYearList.as_view(), name='months_by_year'),
 ]
