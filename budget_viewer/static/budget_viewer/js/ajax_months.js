329800286529d02105df8e73066bee73c0c44a5d
Vitor Filardi <rabelo51@gmail.com>
2019-05-05 17:24:28 -0300

    Create a ajax GET request to pick up months by choosen year from server

--- budget_viewer/static/budget_viewer/js/ajax_months.js
+++ budget_viewer/static/budget_viewer/js/ajax_months.js
@@ -1,3 +1,24 @@
 $('.year__item').click(function(e){
-    console.log($(this).text())
+    let year = $(this).text()
+    // clicking in a year button
+    $.get(
+        `transactions/monthsByYear/${year}`,
+        function(data){
+            console.log(data.months)
+
+            // erase .options__months childs
+            $('.options__months').empty()
+
+            // takes a json data and fill up the .options__months element
+            data.months.forEach(month => {
+                // build a li element with class, attribute and text
+                let $li = $('<li></li>').addClass('month__item').attr("data-id", month.id).text(`${month.name}`)
+                // append to the final of the .options_months element
+               $('.options__months').append($li)
+            });
+        }
+
+    ).fail(function(){
+        console.log("YOU DIED! :(")
+    })
 })
