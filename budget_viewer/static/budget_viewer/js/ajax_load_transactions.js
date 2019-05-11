53763d0335079c14087d7d23d4f04fbea5f537bf
Vitor Filardi <rabelo51@gmail.com>
2019-05-11 13:03:25 -0300

    Add ajax get request to load transactions of a choosen month

--- budget_viewer/static/budget_viewer/js/ajax_load_transactions.js
+++ budget_viewer/static/budget_viewer/js/ajax_load_transactions.js
@@ -22,3 +22,36 @@ $('.year__item').click(function(e){
         console.log("YOU DIED! :(")
     })
 })
+
+$('.options__months').click(function(e){  
+
+    let month_id = $(e.target).attr('data-id')
+    $.get(`transactions/transactionsByMonth/${month_id}`,
+        function(data){
+            console.log(data)
+            // erase previous data
+            $('.budget__body').empty()
+
+            // takes json data and fill up the each budget__cell
+            data.transactions.forEach(transaction => {
+                let $tr = $('<tr></tr>').addClass('budget__row')
+                
+                let $td_statement_number = $('<td></td>').addClass('budget__cell budget__statement_number').text(transaction.fields.statement_number)
+                let $td_date = $('<td></td>').addClass('budget__cell budget__date').text(transaction.fields.date)
+                let $td_flow_method = $('<td></td>').addClass('budget__cell budget__flow_method').text(transaction.fields.flow_method)
+                let $td_origin = $('<td></td>').addClass('budget__cell budget__origin').text(transaction.fields.origin)
+                let $td_amount
+                if (parseFloat(amount) >= 0) {
+                    $td_amount = $('<td></td>').addClass('budget__cell budget__amount budget__amount--income').text(transaction.fields.amount)
+                } else {
+                    $td_amount = $('<td></td>').addClass('budget__cell budget__amount budget__amount--expense').text(transaction.fields.amount)
+                }
+
+                $tr.append($td_statement_number, $td_date, $td_flow_method, $td_origin, $td_amount)
+            })
+            
+        }
+    ).fail(function(){
+        console.log("Não foi possível pegar os dados de transações do mês: " + month_id)
+    })
+})
