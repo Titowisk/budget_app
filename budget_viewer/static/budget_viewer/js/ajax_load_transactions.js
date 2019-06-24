ed2cce6d73cf30470948441c0aa737beaeeb196f
Vitor Filardi <rabelo51@gmail.com>
2019-06-24 10:48:04 -0300

    Add encapsulation to the logic of loading tables using DataTable and $.get

--- budget_viewer/static/budget_viewer/js/ajax_load_transactions.js
+++ budget_viewer/static/budget_viewer/js/ajax_load_transactions.js
@@ -49,51 +49,54 @@ $(document).ready(function(){
         let month_id = $(e.target).attr('data-id')
         $.get(`transactions/transactionsByMonth/${month_id}`,
             function(data){
-                // show table
-                $table.show()
-
-                // load table using DataTable
-                if ( $.fn.dataTable.isDataTable( '#transactions_table' ) ) {
-                    table.destroy() // if table exists, destroy it
-                } 
-                table = $table.DataTable({
-                    // "dom": "<l><t><ip>", // https://datatables.net/reference/option/dom // causes a bug for bootstrap 4 styling
-                    "searching": false,
-                    "data": JSON.parse(data),
-                    // https://datatables.net/reference/option/columns
-                    "columns": [
-                        // {"data": "fields.statement_number"},
-                        {"data": "fields.origin"},
-                        {"data": "fields.amount"},
-                        {"data": "fields.flow_method"},
-                        // {"data": "fields.date"},
-                    ],
-                    // https://datatables.net/reference/option/columnDefs
-                    "columnDefs": [{
-                        "targets": 1,
-                        "createdCell": function (td, cellData, rowData, row, col) {
-                            // change color for differenciation of incomes and expenses
-                            if ( cellData < 0) {
-                                $(td).css('color', '#B30000')
-                            } else {
-                                $(td).css('color', '#04B335')
-                            }
-                        },
-                        
-                    }, 
-                        // https://datatables.net/manual/styling/classes
-                        {"targets": [1, 2], "className": "dt-body-center"}
-                    ]
-                })           
+                // load summary section
+                
+                // load table
+                loadDataTable(data)
                 
             }
         ).fail(function(){
             console.log("Não foi possível pegar os dados de transações do mês: " + month_id)
         })
     })
-    
-    // TODO: function that create textinput filtering for "origin" and "type"
-    let textInputFilter = (colIndex) => {}
-    // TODO: function that create selectinput for "amount" options = ['all', 'incomes', 'expenses']
+    /**
+     * callback function used in $.get to load the DataTable  */ 
+    let loadDataTable = (data) => {
+        // show table
+        $table.show()
 
+        // load table using DataTable
+        if ( $.fn.dataTable.isDataTable( '#transactions_table' ) ) {
+            table.destroy() // if table exists, destroy it
+        } 
+        table = $table.DataTable({
+            // "dom": "<l><t><ip>", // https://datatables.net/reference/option/dom // causes a bug for bootstrap 4 styling
+            "searching": false,
+            "data": JSON.parse(data.transactions),
+            // https://datatables.net/reference/option/columns
+            "columns": [
+                // {"data": "fields.statement_number"},
+                {"data": "fields.origin"},
+                {"data": "fields.amount"},
+                {"data": "fields.flow_method"},
+                // {"data": "fields.date"},
+            ],
+            // https://datatables.net/reference/option/columnDefs
+            "columnDefs": [{
+                "targets": 1,
+                "createdCell": function (td, cellData, rowData, row, col) {
+                    // change color for differenciation of incomes and expenses
+                    if ( cellData < 0) {
+                        $(td).css('color', '#B30000')
+                    } else {
+                        $(td).css('color', '#04B335')
+                    }
+                },
+                
+            }, 
+                // https://datatables.net/manual/styling/classes
+                {"targets": [1, 2], "className": "dt-body-center"}
+            ]
+        })
+    }
 })    
