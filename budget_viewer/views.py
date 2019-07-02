8334e2b5ecbeb7c6e8b7001b3341fcb715e1f2aa
Vitor Filardi <rabelo51@gmail.com>
2019-07-02 15:51:34 -0300

    Add EditCategoryEvent View to handle GET and POST for editing a category from table

--- budget_viewer/views.py
+++ budget_viewer/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import ListView
-from django.http import JsonResponse
+from django.views.generic import ListView, View
+from django.http import JsonResponse, HttpResponse
 
 from bank_statements_reader.models import Transaction, Year, Month
+from .forms import CategoryForm
+
 
 class YearsView(ListView):
     """
@@ -48,6 +50,36 @@ class transactionsByMonth(ListView):
         # https://docs.djangoproject.com/en/2.2/ref/request-response/#serializing-non-dictionary-objects
         return JsonResponse(data, safe=False)
             
+class EditCategoryEvent(View):
+
+    def get(self, request, *args, **kwargs):
+        print("INICIO GET")
+        print(kwargs)
+        # receiveis the pk
+        row_pk = int(kwargs['row_pk'])
+        transaction_to_be_edited = Transaction.objects.get(pk=row_pk)
+        # get the form
+        form = CategoryForm()
+        raw_select_form = str(form['category'])
+        # the selected option will be the current transaction's category (if exists)
+        if (transaction_to_be_edited.category is not None):
+            index = raw_select_form.find('value="{val}"'.format(transaction_to_be_edited.category))
+            select_form = raw_select_form[: index] + 'selected' + raw_select_form[index - 1:]
+            return HttpResponse(select_form)
+        # return the html form to be inserted in popover
+        else:
+
+            return HttpResponse(raw_select_form)
+    
+    # def post(self, request, *args, **kwargs):
+    #     # edit all similar transactions or only the selected one
+    #     # checks if the edit will be for only one or all similars
+    #     # if only one
+    #     # find transaction by pk and update its category
+    #     # else
+    #     # find all transactions with the same origin and update its category
+    #     # handle success and error
+    #     pass
 
 
         
