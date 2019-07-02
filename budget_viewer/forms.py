388b1204cc3b7f0d4c9e5ac5bba5412908f74991
Vitor Filardi <rabelo51@gmail.com>
2019-07-02 15:50:30 -0300

    Add CategoryForm and CategoryFormTests

---
+++ budget_viewer/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from bank_statements_reader.models import Category
+
+# Django Form Fields
+# https://docs.djangoproject.com/en/2.2/ref/forms/fields/
+
+class CategoryForm(forms.Form):
+
+    category = forms.ChoiceField(choices=Category.TRANSLATION_PTBR)
+
+    # https://docs.djangoproject.com/en/2.2/ref/forms/widgets/#styling-widget-instances
+    category.widget.attrs.update({'class': 'custom-select'}) # add bootstrap class to <select>
