245a32fe0a185c57b7cd0f91e66c618262ea6f9d
Vitor Filardi <rabelo51@gmail.com>
2019-07-09 20:00:53 -0300

    Change id name of the select form

--- budget_viewer/forms.py
+++ budget_viewer/forms.py
@@ -9,4 +9,4 @@ class CategoryForm(forms.Form):
     category = forms.ChoiceField(choices=Category.TRANSLATION_PTBR)
 
     # https://docs.djangoproject.com/en/2.2/ref/forms/widgets/#styling-widget-instances
-    category.widget.attrs.update({'class': 'custom-select'}) # add bootstrap class to <select>
+    category.widget.attrs.update({'class': 'custom-select', 'id': 'edit-category-select'}) # add bootstrap class to <select>
