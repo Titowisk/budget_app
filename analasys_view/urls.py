da88b8bd42010bfe61cd51a2058afe3c9a2cf6f2
Vitor Filardi <rabelo51@gmail.com>
2019-07-21 12:56:21 -0300

    Create view sample using plotly

--- analasys_view/urls.py
+++ analasys_view/urls.py
@@ -4,7 +4,7 @@ from . import views
 # https://docs.djangoproject.com/en/2.2/ref/urls/#path
 
 urlpatterns = [
-    path('by_income', name='by_income'),
-    path('by_category', name='by_category'),
-    path('by_origin', name='by_origin')
+    path('by_income', views.AnalisysByIncomeView.as_view(), name='by_income'),
+    # path('by_category', name='by_category'),
+    # path('by_origin', name='by_origin')
 ]
